9bf8b9fa1c78c688359b1d121939d9518f7e26f5
Huowsng <huongvlt007@gmail.com>
2024-01-01 18:49:04 +0700

    updatett

--- backend/client.py
+++ backend/client.py
@@ -17,10 +17,16 @@ def main(args):
     print('Data: {} ... {}'.format(data[:50], data[len(data)-52:]))
 
     headers = {"content-type": "application/json"}
-    json_response = requests.post('http://localhost:8501/v1/models/srgan:predict', data=data, headers=headers)
-    predictions = json.loads(json_response.text)['predictions']
-    out = np.array(predictions, dtype=np.uint8)
-    save_image(out[0], args[1])
+    json_response = requests.post('https://aa3b-2402-800-6205-3a3b-59e8-93f8-e4db-fab9.ngrok-free.app/v1/models/srgan:predict', data=data, headers=headers)
+    if json_response.status_code == 200:
+        try:
+            predictions = json.loads(json_response.text)['predictions']
+            out = np.array(predictions, dtype=np.uint8)
+            save_image(out[0], args[1])
+        except json.decoder.JSONDecodeError as e:
+            print(f"Error decoding JSON response: {e}")
+    else:
+        print(f"Error from API: {json_response.status_code} - {json_response.text}")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
